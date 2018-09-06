86824d958abf3f958ec4eb31e4521636b9fc40c8
John Freeman <john.freeman.opensource@gmail.com>
2018-09-06 18:15:54 +0100

    Added script for creating the next version (#8)
    As part of the release process.

---
+++ next-version.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! -x "$(command -v git)" ]]; then
+    echo 'Error: git is not installed.' >&2
+    exit 1
+fi
+if [[ ! -x "$(command -v hub)" ]]; then
+    echo 'Error: hub is not installed.' >&2
+    exit 1
+fi
+if [[ ! -x "$(command -v shellcheck)" ]]; then
+    echo 'Error: shellcheck is not installed.' >&2
+    exit 1
+fi
+if [[ "$(git status --untracked-files --short)" != '' ]]; then
+    echo 'Error: you have uncommited changes' >&2
+    exit 1
+fi
+
+shellcheck moleculew
+shellcheck next-version.sh
+
+WRAPPER_VERSION="$(./moleculew wrapper-version)"
+RELEASE_VERSION="${WRAPPER_VERSION%%-dev*}"
+
+read -r -p "Release version [$RELEASE_VERSION]? " USER_RELEASE_VERSION
+if [[ "$USER_RELEASE_VERSION" != '' ]]; then
+    RELEASE_VERSION="$USER_RELEASE_VERSION"
+fi
+
+PATCH_VERSION="${RELEASE_VERSION##*.}"
+NEXT_PATCH_VERSION="$((PATCH_VERSION + 1))"
+NEXT_VERSION="${RELEASE_VERSION%.*}.$NEXT_PATCH_VERSION-dev"
+
+set -x
+
+git checkout -b "release-$RELEASE_VERSION"
+
+sed --in-place \
+    "s/^WRAPPER_VERSION=.*/WRAPPER_VERSION=$RELEASE_VERSION/" moleculew
+
+git add --verbose moleculew
+
+git commit --message="\
+Prepare release $RELEASE_VERSION
+"
+
+hub pull-request --push --base master --message="\
+Release $RELEASE_VERSION
+"
+
+git checkout -b "dev-$NEXT_VERSION"
+
+sed --in-place \
+    "s/^WRAPPER_VERSION=.*/WRAPPER_VERSION=$NEXT_VERSION/" moleculew
+
+git add --verbose moleculew
+
+git commit --message="\
+Prepare for next development iteration
+"
+
+hub pull-request --push --base develop --message="\
+Develop $RELEASE_VERSION
+"
